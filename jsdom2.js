bee84eaab52071f3a793e766d5fe930354152e24
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2023-12-02 18:16:49 -0500

    "added class 'important' to muscle milk"

--- jsdom2.js
+++ jsdom2.js
@@ -28,6 +28,34 @@ myFunction2()
 
 //4. Clear the list and programmatically add items from the array ['protein powder', 'muscle milk', 'power bars']
 
-
+function myFunction3(){
+    const element = document.createElement("li");
+    const textNode = document.createTextNode("protein powder");
+    element.appendChild(textNode);
+  
+    const list = document.getElementById ("list");
+    list.replaceChild(element, list.children[0]);
+  
+    const element2 = document.createElement("li");
+    const textNode2 = document.createTextNode("muscle milk");
+    element2.appendChild(textNode2);
+  
+    const list2 = document.getElementById("list");
+    list2.replaceChild(element2, list2.children[1]);
+  
+    const element3 = document.createElement("li");
+    const textNode3 = document.createTextNode("power bars");
+    element3.appendChild(textNode3);
+  
+    const list3 = document.getElementById("list");
+    list3.replaceChild(element3, list3.children[2]);
+  }
+myFunction3()
 
 //5. Add the class 'important' to the muscle milk item.
+
+function myFunction4(){
+    const addClass = document.getElementById ("list").children[1];
+    addClass.classList.add("important");
+}
+myFunction4()
